a3ddd3476095e72aaf56adb70be6bfa99a978f9d
Shahryar786 <66066742+Shahryar786@users.noreply.github.com>
2020-05-29 11:20:13 +0500

    Add files via upload

---
+++ SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void NextLevelButton(int index)
+    {
+        Application.LoadLevel(index);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void ExitLevel()
+    {
+        Application.LoadLevel("Home");
+    }
+    public void Retry1()
+    {
+        Application.LoadLevel("Level 1");
+    }
+
+    public void Retry2()
+    {
+        Application.LoadLevel("Level 2");
+    }
+
+}
